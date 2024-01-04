c88c54ad59bd3fc6514311bed6d290e94386a1f7
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2024-01-04 16:49:34 +0300

    8.hafta 2.saat

---
+++ OpenTelemetry.API/ActivitySourceProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTelemetryApp
+{
+    internal class ActivitySourceProvider
+    {
+        public static ActivitySource ActivitySource = new ActivitySource("ActivitySourceToConsole");
+
+      
+    }
+}
